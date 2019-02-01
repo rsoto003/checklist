ebf0398da58faa38460654e43aff0fa2d78c8da8
Ryan Soto <rsoto003@ucr.edu>
2019-01-31 19:50:10 -0800

    changing settings

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {
-        mongoURI: 'mongodb://ryan:Rps04trojans/@ds219055.mlab.com:19055/checklist-prod'
+        mongoURI: 'mongodb://ryan:rps04trojans@ds219055.mlab.com:19055/checklist-prod'
     }
 } else {
     module.exports = {
